8a3f7c081eab26049717903c820a64e5a58ff1b2
KIMPPT <dudghks9303@gmail.com>
2023-04-02 00:48:23 +0900

    04020048

--- ex-react/src/components/LoginComp.jsx
+++ ex-react/src/components/LoginComp.jsx
@@ -33,8 +33,11 @@ export class LoginComp extends Component {
           {login ? name : ''}
         </h1>
         {/*두번째 방법 : display의 값만 삼항연산자를 넣어 표현 */}
+        {/*
+        
+        */}
         <div style={{ display: login ? 'none' : 'block' }}>
-        {/*div className={login?"loginon":""} 를 입력하면 바로 위의 식과 동일하다. import를 만들어야 적용이 되며 위의 import의 색이 연한 색이 되어도 신경 쓰지 않아도 된다*/}
+        {/* <div className={login?"loginon":""}> 를 입력하면 바로 위의 식과 동일하다. import를 만들어야 적용이 되며 위의 import의 색이 연한 색이 되어도 신경 쓰지 않아도 된다*/}
           <h3>이름을 입력하세요</h3>
           <input type="text" onChange={this.inputChange} />
           <button onClick={this.loginMethod}>확인</button>
