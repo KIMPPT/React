b2410136f83a82b4718f615e4463587462c768a7
KIMPPT <dudghks9303@gmail.com>
2023-04-04 16:41:41 +0900

    04041641

--- react-app/src/components/FilterableProductTable.jsx
+++ react-app/src/components/FilterableProductTable.jsx
@@ -1,41 +1,42 @@
-import React, { Component } from 'react'
-import SearchBar from './SearchBar'
-import ProductTable from './ProductTable'
-import productData from '../data/product.json'
+import React, { Component } from "react";
+import SearchBar from "./SearchBar";
+import ProductTable from "./ProductTable";
+import productData from "../data/product.json";
 //1. 하향식으로 작성(부모부터 작성)
 //2. 정적 데이터를 이용해서 작성
 //3. state와 props 구분해서 작성
 export class FilterableProductTable extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      searchText:"",
-      isOnlyStock: false
-    }
+    this.state = {
+      searchText: "",
+      isOnlyStock: false,
+    };
   }
   //toggleStock 메소드를 실행할때마다 this.state.isOnlyStock T/F 로 바꿈
-  toggleStock=()=>{
-    this.setState({isOnlyStock:!this.state.isOnlyStock})
-    console.log("메소드실행")
-  }
+  toggleStock = () => {
+    this.setState({ isOnlyStock: !this.state.isOnlyStock });
+    console.log("메소드실행");
+  };
   render() {
     console.log(productData);
     //isOnlyStock이 true일 때 productData가 stock이 true인것만 배열로 만들기
-    let checkedProduct=productData.filter((product)=>product.stocked)
+    let checkedProduct = productData.filter((product) => product.stocked);
     return (
       <div>
         {/*검색어, 체크박스에 관한 값 */}
-        <SearchBar 
-        
-        isOnlyStock={this.state.isOnlyStock}
-        //props 값으로 메소드를 작성해서 전달
-        toggleStock={this.toggleStock}
+        <SearchBar
+          isOnlyStock={this.state.isOnlyStock}
+          //props 값으로 메소드를 작성해서 전달
+          toggleStock={this.toggleStock}
         />
         {/*가져온 데이터 값을 보여줄 공간 */}
-        <ProductTable products={this.state.isOnlyStock?checkedProduct:productData}/>
+        <ProductTable
+          products={this.state.isOnlyStock ? checkedProduct : productData}
+        />
       </div>
-    )
+    );
   }
 }
 
-export default FilterableProductTable
+export default FilterableProductTable;
--- react-app/src/components/SearchBar.jsx
+++ react-app/src/components/SearchBar.jsx
@@ -2,17 +2,25 @@ import React, { Component } from "react";
 
 //글자를 입력 받아서 필터링 하는 컴포넌트
 export class SearchBar extends Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
   }
   render() {
-    let {isOnlyStock,toggleStock}=this.props;
+    let { isOnlyStock, toggleStock } = this.props;
     return (
       <div>
-        <input type="text" placeholder="Search..."></input>
+        <input
+          type="text"
+          placeholder="Search..."
+          defaultChecked={isOnlyStock}
+        />
         <br />
-        <input type="checkbox" checked={isOnlyStock}/>
-        <label htmlFor="" onClick={toggleStock}>Only show products in stock</label>
+        {/* checkbox/radio의 값을 가져 올 때 readOnly를 이용해서 바뀌는 값을 출력하는 용도로 사용 할 수 있다*/}
+        <input type="checkbox"
+        checked={isOnlyStock}
+        readOnly
+        onClick={toggleStock} />
+        <label htmlFor="">Only show products in stock</label>
       </div>
     );
   }
