fffaf4f17607d5971296f94c46ff2fb4d2a8f79d
KIMPPT <dudghks9303@gmail.com>
2023-04-04 12:41:30 +0900

    04041241

---
+++ react-app/src/App.js
@@ -0,0 +1,12 @@
+import FilterableProductTable from './components/FilterableProductTable';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
