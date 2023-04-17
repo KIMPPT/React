7bfcff2e0c67abdb59e0e436d932b74fc370dbbf
KIMPPT <dudghks9303@gmail.com>
2023-04-17 12:59:29 +0900

    04171259

--- router-context/src/context/DataContext.jsx
+++ router-context/src/context/DataContext.jsx
@@ -1,7 +1,33 @@
 import React from "react";
+import { useState } from "react";
+let DataContext = React.createContext("");
 
-let DataContext=React.createContext("");
-
-let DataProvider=({children})=>{
-    
-}
+let DataProvider = ({ children }) => {
+  //데이터를 저장 및 수정하기 위해 useState 사용
+  let [boardlist, setBoardlist] = useState([
+    {
+      id: 1,
+      title: "첫번째 게시물입니다",
+      content: "내용을 작성하였습니다",
+      date: "2023-04-14",
+      writer: "green",
+    },
+    {
+      id: 2,
+      title: "두번째 게시물입니다",
+      content: "내용을 작성하였습니다",
+      date: "2023-04-14",
+      writer: "green",
+    },
+  ]);
+  //value에 담을 데이터 정리
+  let value = {
+    state: { boardlist },
+    action: { setBoardlist },
+  };
+  return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
+};
+//값을 전달하기 위해 감싸는 컴포넌트
+export {DataProvider}
+//값을 사용하기 위해 호출하는 컴포넌트
+export default DataContext;
--- router-context/src/page/Board.jsx
+++ router-context/src/page/Board.jsx
@@ -1,10 +1,13 @@
-import React, { useEffect } from 'react'
-import { useParams, useNavigate } from 'react-router-dom'
+import React, { useEffect,useContext } from 'react'
+import { useParams, useNavigate} from 'react-router-dom'
 
 import data from '../data/dummy.json'
-
+import DataContext from '../context/DataContext';
 // id로 구분하기위해 board에 data의 내용이 필요함
 export default function Board() {
+  let value=useContext(DataContext);
+  console.log(value);
+
   // useNavigate를 사용하면 함수를 이용해서 화면이동가능
   const navigate = useNavigate();
 
@@ -14,11 +17,12 @@ export default function Board() {
   // find로 가져온값은 배열 안에 있는 하나의 값
   // find로 값을 찾지 못할 경우 undefined 출력
   // undefined의 값에 속성을 찾으려고하면 >> 오류!
-  const boardData = data.find((d)=>(d.id == id))
+  const boardData = value.state.boardlist.find((d)=>(d.id == id))
+  //data 대신 value.state.boardlist로 접근
 
   // useEffect를 사용해서 boardData값이 undefind면 
   // 오류 페이지 컴포넌트로 이동 혹은 목록으로 이동
-  // 두번쨔 인자값이 빈 배열이라면 컴포넌트 생성시에 실행
+  // 두번째 인자값이 빈 배열이라면 컴포넌트 생성시에 실행
   useEffect(()=>{
     if (boardData == undefined) {
         navigate('/boardlist');
