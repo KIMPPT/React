c3f987851b7892f0a26d994041bab1fdecb34f90
KIMPPT <dudghks9303@gmail.com>
2023-04-19 13:19:49 +0900

    04191319

--- router-context/src/components/CommentComp.jsx
+++ router-context/src/components/CommentComp.jsx
@@ -5,6 +5,7 @@ import React from 'react'
 export default function CommentComp(props) {
   //props로 전달한 comment를 구조분해를 통해서 쓰기 쉽게 작성
   let {writer,text,date,cid}=props.comment;
+  //작성한 메서드를 들고와서 사용
   let {deleteComment}=props;
   return (
     <div>
@@ -12,6 +13,8 @@ export default function CommentComp(props) {
       <p>{text}</p>
       <span>{date}</span>
       <button
+      //메서드를 사용 할 때 cid를 전달해야 함
+      //()=>{}로 감싸서 사용
       onClick={()=>{deleteComment(cid)}}>X</button>
     </div>
   )
